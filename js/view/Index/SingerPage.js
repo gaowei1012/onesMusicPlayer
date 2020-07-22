4e64c2bb4c64eadf219b0a2106b2ece7e67e0ac9
“gaowei1012” <mingwei2222@126.com>
2020-07-22 12:57:57 +0800

    fix

--- js/view/Index/SingerPage.js
+++ js/view/Index/SingerPage.js
@@ -150,7 +150,7 @@ const styles = StyleSheet.create({
     height: px2dp(45),
     alignSelf: center,
     marginTop: px2dp(4),
-    backgroundColor: defaultBackgroundColor,
+    // backgroundColor: defaultBackgroundColor,
     marginBottom: px2dp(2),
     flexDirection: row,
     alignItems: center,
