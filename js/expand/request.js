96321741c856a50f6c346967923a17ffd5378915
“gaowei1012” <mingwei2222@126.com>
2020-07-23 13:18:32 +0800

    fix  增加接口token支持

--- js/expand/request.js
+++ js/expand/request.js
@@ -2,17 +2,16 @@ import qs from 'qs';
 import axios from 'axios';
 import {base_url} from './api';
 
-export function request(url, data = {}, method, token) {
+export function request(url, token) {
   return new Promise((resolve, reject) => {
     axios({
       url: url,
       baseURL: base_url,
-      data: qs.stringify(data),
       headers: {
         'Content-Type': 'application/json',
         'token': token
       },
-      method: method,
+      method: "GET",
     })
       .then(res => {
         resolve(res.data);
